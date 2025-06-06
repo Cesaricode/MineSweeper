d9ac63715d169d09586b763c2dac72a25beb8c3d
Alex Louwe <alex.louwe@hva.nl>
2025-06-06 20:02:51 +0200

    Removes dist folder from git.

--- build/index.js
+++
@@ -1,4 +0,0 @@
-import { renderBoard } from "./ui.js";
-window.addEventListener("DOMContentLoaded", () => {
-    renderBoard();
-});
--- build/util.js
+++
@@ -1,8 +0,0 @@
-export const directions = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [0, -1], [0, 1],
-    [1, -1], [1, 0], [1, 1],
-];
-export function isInBounds(x, y, numRows, numCols) {
-    return x >= 0 && x < numRows && y >= 0 && y < numCols;
-}
